609c5fdd1b2124fcfd938b79173377de2deebb2e
voeunlyna-cyber <voeunlyna@gmail.com>
2026-06-24 16:17:30 +0700

    hh

--- model/Clear_BS.java
+++ model/Clear_BS.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Clear_BS {
+    String variable = "123456789";
+    void set_clear(){
+        variable = "";
+    }   
+    void set_backspace(){
+        variable = variable.substring(0, variable.length() - 1);
+    }       
+}
+String get_variable(){
+    return variable;
+}
+public static void main(String[] args) {
+    Clear_BS cb = new Clear_BS();
+    System.out.println(cb.get_variable());
     
+    cb.set_backspace();
+    System.out.println(cb.get_variable());
+
+    cb.set_backspace();
+    System.out.println(cb.get_variable());
+
+    cb.set_clear();
+    System.out.println(cb.get_variable());
 }
--- model/Number_input.java
+++ model/Number_input.java
@@ -1,5 +1,69 @@
 package model;
 
 public class Number_input {
+    String Variable;
+    void set_zero(){
+        Variable = Variable + "0";
+    }
+
+    void set_one(){
+        Variable = "1";
+    }
+
+    void set_two(){
+        Variable = Variable + "2";
+    }
+
+    void set_three(){
+        Variable = "3";
+    }
+
+    void set_four(){
+        Variable = Variable + "4";
+    }
+
+    void set_five(){
+        Variable = Variable + "5";
+    }
+
+    void set_six(){
+        Variable = Variable + "6";
+    }
+
+    void set_seven(){
+        Variable = Variable + "7";
+    }
+
+    void set_eight(){
+        Variable = Variable + "8";
+    }
+
+    void set_nine(){
+        Variable = Variable + "9";
+    }
+
+    String get_variable(){
+        return Variable;
+        
+    }
+    public static void main(String[] args) {
+        Number_input number_input = new Number_input();
+        number_input.set_zero();
+        number_input.set_one();
+        number_input.set_two();
+        number_input.set_three();
+        number_input.set_four();
+        number_input.set_five();
+        number_input.set_six();
+        number_input.set_seven();
+        number_input.set_eight();
+        number_input.set_nine();
+        System.out.println(number_input.get_variable());
+        Memory m = new Memory();
+        m.set_first_variable(number_input.get_variable());
+        System.out.println(m.get_first_variable());
+
+
+    }
     
 }
